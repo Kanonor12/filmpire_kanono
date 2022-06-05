b016b850e4cde5ca68704ca2d514ad9146cffeac
Kanono Leronti Ramashamole <kanono@gmail.com>
2022-06-05 14:13:07 +0200

    Authentication completed

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import genreOrCategoryReducer from "../features/currentGenreOrCategory";
 import { tmdbApi } from "../services/TMDB";
+import userReducer from '../features/auth';
 
 export default configureStore ({
     reducer: {
       [tmdbApi.reducerPath]: tmdbApi.reducer,
       currentGenreOrCategory: genreOrCategoryReducer,
+      user: userReducer,
     },
     middleware: getDefaultMiddleware =>
     getDefaultMiddleware({
